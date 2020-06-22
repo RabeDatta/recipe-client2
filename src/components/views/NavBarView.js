c2bfcc8344af1f27214fe8645d642d4c1fd384cf
RabeDatta <robedutt@gmail.com>
2020-06-22 17:36:20 -0400

    working background image and nav bar

--- src/components/views/NavBarView.js
+++ src/components/views/NavBarView.js
@@ -10,10 +10,10 @@ const NavBarView = (props) => {
      
       <ul className="right hide-on-med-and-down">
         <li> <Link to="/home"> Home</Link> </li> 
-        <li> <Link to="/login"> Login </Link> </li>
-        <li> <Link to="/signup" > Signup </Link> </li>
         <li> <Link to="/recipes" > Recipes </Link> </li>
-        <li> <Link to="/reviews" > Reviews </Link> </li>
+        <li> <Link to="/reviews" > Reviews </Link> </li> 
+        <li> <Link to="/login"> Login </Link> </li> 
+        <li> <Link to="/signup" > Signup </Link> </li>
       </ul>
     </div>
     </nav>
