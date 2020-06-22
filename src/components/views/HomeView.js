0e42ffac0d2ce478c90743e15478f672354ff6da
RabeDatta <robedutt@gmail.com>
2020-06-22 16:14:46 -0400

    working background and nav bar

--- src/components/views/HomeView.js
+++ src/components/views/HomeView.js
@@ -1,12 +1,16 @@
 import React from "react";
+import "./styles/HomeView.css";
 
 
 const HomeView = (props) => {
   return (
     <home>
+      <header>
       <div className="home">
-        <h1>Welcome to InstaRecipe!</h1>
+        <h1 className="home-header">Welcome to InstaRecipe!</h1>
+        <p>  </p>
       </div>
+      </header>
     </home>
   );
 };
--- src/components/views/NavBarView.js
+++ src/components/views/NavBarView.js
@@ -2,57 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
-
-/*
-const NavLink = () => {
-  return (
-    <nav className="nav-wrapper grey darken-3">
-      <div className="container">
-        <Link to='/' className="brand-logo">MarioPlan</Link>
-        <SignedInLinks />
-        <SignedOutLinks />
-      </div>
-    </nav>
-  )
-}
-
-
-const SignedInLinks = () => {
-  return (
-    <div>
-      <ul className="right">
-        <li><NavLink to='/'>New Project</NavLink></li>
-        <li><NavLink to='/'>Log Out</NavLink></li>
-        <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
-      </ul>
-    </div>
-  )
-}
-
-
-const SignedOutLinks = () => {
-  return (
-    <div>
-      <ul className="right">
-        <li><NavLink to='/'>Signup</NavLink></li>
-        <li><NavLink to='/'>Login</NavLink></li>
-      </ul>
-    </div>
-  )
-}
-
-
-export default NavBarView;
-*/
-
-
-
 const NavBarView = (props) => {
   return (
-    <nav class="naxbar-fixed">
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul className="nav-links" id="nav-mobile" class="right hide-on-med-and-down">
+    <nav className="nav-wraper indigo">
+    <div className="container">
+      <a href="#" className="brand-logo left">Site Title</a>
+     
+      <ul className="right hide-on-med-and-down">
         <li> <Link to="/home"> Home</Link> </li> 
         <li> <Link to="/login"> Login </Link> </li>
         <li> <Link to="/signup" > Signup </Link> </li>
